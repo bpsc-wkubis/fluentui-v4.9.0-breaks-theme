75959477393f54750a6232ae599bb38853bf9bb6
WKubis_forterro <wiktor.kubis@forterro.com>
2024-07-09 15:41:22 +0200

    init

---
+++ BlazorApp2/wwwroot/theme.js
@@ -0,0 +1,19 @@
+﻿import {
+    accentBaseColor,
+    SwatchRGB,
+    fillColor,
+    neutralLayer3,
+    bodyFont,
+    density
+} from "/_content/Microsoft.FluentUI.AspNetCore.Components/Microsoft.FluentUI.AspNetCore.Components.lib.module.js";
+
+const defaultAccentBaseColor = SwatchRGB.create(33, 183, 179);
+const defaultDensity = 2;
+
+function initializeTheme() {
+    accentBaseColor.withDefault(defaultAccentBaseColor);
+    fillColor.withDefault(neutralLayer3);
+    density.withDefault(defaultDensity);
+};
+
+initializeTheme();
